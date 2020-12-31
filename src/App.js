d3c129d0d09bc0ce1592c5b6ff0f281900555624
dianavile <diana.vile@gmail.com>
2020-12-31 12:02:28 +0100

    #3: searchBar Component

--- src/App.js
+++ src/App.js
@@ -2,29 +2,31 @@ import React from 'react';
 import { Grid } from "@material-ui/core";
 import './App.css';
 import youtube from '../src/api/youtube';
+import { SearchBar } from './components';
 
 //create classbased component with state
 class App extends React.Component {
   //add state
-  state = {
-    videos: [],
-    selectedVideo:null
-  }
+  // state = {
+  //   videos: [],
+  //   selectedVideo:null
+  // }
   //add methods()
-  handleVideoSelect = (video) => {
-    this.setState({ selectedVideo: video})
-  }
+  // handleVideoSelect = (video) => {
+  //   this.setState({ selectedVideo: video})
+  // }
 
   handleSubmit = async (searchTerm) => {
     const response = await youtube.get('search', {
       params: {
-        part: 'snippet' ,
+        part: "snippet",
         maxResults: 5,
-        key: 'KEY', 
-        q: searchTerm
-        }
+        key: "AIzaSyCn0H63y8Z28n7g-NkLhVrDpLGoDuQs6VM",
+        q: searchTerm,
+      }
     })
-    this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] })
+    console.log(response.data.items);
+    //this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] })
   }
 
   render() {
@@ -36,21 +38,27 @@ class App extends React.Component {
         justify="center"
         alignItems="center">
         <Grid item xs={12}>
-          <Grid container spacing={10}>
-            <Grid item xs={12}>
-              <div>
-              SEARCHBAR
-              </div>
+            <Grid container spacing={10}>
+            <Grid item xs={12}> 
+                <SearchBar onFormSubmit={this.handleSubmit}/>
             </Grid>
-            <Grid item xs={8}>
-              <div>
-                VIDEODETAIL
-              </div>
+            <Grid item xs={8} className="videoDetail">
+              VIDEODETAIL
             </Grid>
-            <Grid item xs={4}>
-              <div>
-              VIDEOLIST
-              </div>
+            <Grid item xs={4} className="videoList">
+                VIDEOLIST
+                <Grid item xs={12} className="videoItem">
+                  VIDEOITEM
+                </Grid>
+                <Grid item xs={12} className="videoItem">
+                  VIDEOITEM
+                </Grid>
+                <Grid item xs={12} className="videoItem">
+                  VIDEOITEM
+                </Grid>
+                <Grid item xs={12} className="videoItem">
+                  VIDEOITEM
+                </Grid>
             </Grid>
           </Grid>
         </Grid>
--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,11 +1,5 @@
 import axios from 'axios';
-const KEY = "AIzaSyCn0H63y8Z28n7g-NkLhVrDpLGoDuQs6VM";
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-    part: "snippet",
-    maxResults: 5,
-    key: KEY
-  },
 });
